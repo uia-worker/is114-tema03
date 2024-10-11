57d3c6a203ab4f561866e389f68d1be0035c93c7
Janis Gailis <janis.gailis@uia.no>
2024-10-11 14:22:00 +0200

    eksempel på lese inn fra google sheets

---
+++ src/python/python-gdrive.py
@@ -0,0 +1,27 @@
+import gspread as gs
+import pandas as pd
+import numpy as np
+import altair as alt
+
+# Få tilgang til Google Cloud (gc)
+gc = gs.api_key("AIzaSyBKrUqkbU3gsEujXEh8N3uQTN7fM0Dpg3I")
+# Laste inn hele regnarket fra Google Drive i variabelen sh (spread[sh]eet)
+sh = gc.open_by_key('1RYN0i4Zx_UETVuYacgaGfnFcv4l9zd9toQTTdkQkj7g')
+# Velge et spesifikt ark fra regnarket (wsh - [w]ork[sh]eet)
+wsh = sh.worksheet("o1-oppg3")
+print(sh.worksheets())
+# Laste inn (binde til navnet df) data fra wsh i dataramme (df - [d]ata[f]rame)
+df = pd.DataFrame(wsh.get_all_records())
+# Eksempel på bruken av det populære matematikk-pakken numpy
+# Binder data til navnet array
+array = np.array(wsh.get_all_values())
+
+
+# Presentere data
+chart = alt.Chart(df).mark_bar(
+).encode(
+    x = 'age:Q',     
+    y = 'first_name:N' 
+)
+ 
+chart.save('bar-chart.html')
