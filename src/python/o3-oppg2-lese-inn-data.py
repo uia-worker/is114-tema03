6c9e22973185ce1b15c17cbf04f36f43ad2e0489
Janis Gailis <janis.gailis@uia.no>
2024-10-11 14:49:57 +0200

    lese inn data fra lokal excel-fil, må være i samme mappe som programmet

---
+++ src/python/o3-oppg2-lese-inn-data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+kgdata = pd.read_excel("ssb-barnehager-2015-2023-alder-1-2-aar.xlsm", sheet_name="KOSandel120000",
+                       header=3,
+                       names=['kom','y15','y16','y17','y18','y19','y20','y21','y22','y23'],
+                       na_values=['.', '..'])
+
+
