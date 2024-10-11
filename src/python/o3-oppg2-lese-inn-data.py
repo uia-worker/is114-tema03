b66a452c72378cd813a4e1dd9e67a7909aa6006e
Janis Gailis <janis.gailis@uia.no>
2024-10-11 16:31:07 +0200

    endringer i flere filer

--- src/python/o3-oppg2-lese-inn-data.py
+++ src/python/o3-oppg2-lese-inn-data.py
@@ -5,4 +5,4 @@ kgdata = pd.read_excel("ssb-barnehager-2015-2023-alder-1-2-aar.xlsm", sheet_name
                        names=['kom','y15','y16','y17','y18','y19','y20','y21','y22','y23'],
                        na_values=['.', '..'])
 
-
+    
--- src/python/sum_list.py
+++ src/python/sum_list.py
@@ -1,3 +1,5 @@
+import time
+import sys
 """
 
 For å kunne utføre testing må du installere pytest module.
@@ -11,12 +13,33 @@ OBS! ">>>" er strengen som representerer promt og er ikke en del av kommandoen
 
 """
 
-def sum_list(numlist : list) -> float:
+def sum_list(numlist : list[int]) -> int:
     """summer en liste med tall"""
     total = 0
     for num in numlist:
         total = total + num
     return total
 
+def sum_list_rec(numlist : list[int], total : int = 0) -> int:
+    if len(numlist) != 0:
+        first = numlist[0]
+        del numlist[0]
+        total = first + sum_list_rec(numlist, total)
+    return total
+
 def test_sum_list():
     assert sum_list([1, 2, 3]) == 6
+    assert sum_list_iter([1, 2, 3]) == 6
+    assert sum_list_iter([-1, 2, 3]) == 4
+    
+sys.setrecursionlimit(1999)
+start_time = time.time()
+sum_list_rec(list(range(0,1600)))
+end_time = time.time()
+elapsed = end_time - start_time
+elapsed_sc_not = "{:e}".format(elapsed)
+print("Tiden for rekursiv version er:", elapsed_sc_not, "sekunder")
+start_time = time.time()
+sum_list(list(range(0,1600)))
+end_time = time.time()
+print("Tiden for iterasjonsversjon er:", end_time - start_time, "sekunder")
